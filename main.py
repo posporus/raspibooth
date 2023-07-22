7c6f5dc095d1a6585d35063f33153d2dec9e356e
posporus <git@posporus.de>
2023-07-22 22:32:34 +0200

    playing around

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from sys import settrace
+# from sys import settrace
 
-from rpi_ws281x import Color
-from ringlight import fx
-from ringlight.strip import strip
-import time
+# from rpi_ws281x import Color
+# from ringlight import fx
+# from ringlight.strip import strip
+# import time
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     
-    fx.theaterChase.loop = -1
-    #theaterChase(Color(100,100,100))
-    fx.rainbow.loop = -1
-    fx.rainbow.brightness =20
-    fx.rainbow()
-    fx.blackout()
-    time.sleep(1)
-    fx.solid(Color(2,55,100))
+#     fx.theaterChase.loop = -1
+#     #theaterChase(Color(100,100,100))
+#     fx.rainbow.loop = -1
+#     fx.rainbow.brightness =20
+#     fx.rainbow()
+#     fx.blackout()
+#     time.sleep(1)
+#     fx.solid(Color(2,55,100))
+
+from src.load_config import config
+
+print(config)
