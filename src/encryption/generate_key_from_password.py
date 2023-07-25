0704ace8ea3ec049f3f9255139ba73b7a2ab1056
posporus <git@posporus.de>
2023-07-25 12:03:01 +0200

    basic encryption working, tests, successful.

---
+++ src/encryption/generate_key_from_password.py
@@ -0,0 +1,19 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+
+def generate_key_from_password(password:str,salt:bytes):
+
+    # Use PBKDF2HMAC to derive a key from the password
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode('utf-8'))
+
+    return key
