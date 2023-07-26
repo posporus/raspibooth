1e6f3f28b0e1fd42fb175ca5cf281d18691886c3
posporus <git@posporus.de>
2023-07-26 16:56:58 +0200

    camera_picamera and mock_camera doing.

--- tests/mocks/mock_camera.py
+++ tests/mocks/mock_camera.py
@@ -1,19 +1,32 @@
 from src.abstract.camera import Camera
-from tests.mocks.create_image import createImage
-from tests.mocks.create_video import createVideo
 from pathlib import Path
+import subprocess
 
 
 class MockCamera(Camera):
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self, width: int, height: int, fps=30) -> None:
         self._width = width
         self._height = height
-    
-    def take_picture(self, fp: Path):
-        return createImage(self.width, self.height, fp)
+        self._fps = fps
 
-    def take_video(self, fp: Path, fps=24, duration_ms=500):
-        return createVideo(self.width, self.height, fp, fps, duration_ms)
+    def take_video(self, fp: Path, duration_ms=500):
+        num_frames = int(self.fps*(duration_ms/1000))
+
+        dst_path = str(fp)
+        src_path = 'tests/fixtures/testvideo_540x960.h264'
+        command = [
+            'ffmpeg',
+            '-i', src_path,
+            '-vframes', str(num_frames),
+            '-c:v', 'copy',
+            dst_path
+        ]
+
+        if subprocess.run(command).returncode != 0:
+            print(f'Error occurred while copying video to {dst_path}')
+            return False
+
+        return True
 
     @property
     def width(self):
@@ -22,3 +35,7 @@ class MockCamera(Camera):
     @property
     def height(self):
         return self._height
+
+    @property
+    def fps(self):
+        return self._fps
