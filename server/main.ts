1d1257e86c542ef93f00217c4da96ccb08fd904b
posporus <git@posporus.de>
2023-08-04 11:51:44 +0200

    twind v1

--- server/main.ts
+++ server/main.ts
@@ -3,13 +3,12 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-
 import "$std/dotenv/load.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import twindPlugin from "$fresh/plugins/twind.ts";
+import twindPlugin from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
