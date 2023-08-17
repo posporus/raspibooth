4173f3d9fa54eeb2885a38519b39533a3f41f337
posporus <git@posporus.de>
2023-08-17 19:49:40 +0200

    improvements

--- server/islands/LeaveMail.tsx
+++ server/islands/LeaveMail.tsx
@@ -13,7 +13,7 @@ export default function LeaveMail ({ fileId }: LeaveMailProps) {
         <div class="hero min-h-screen bg-base-200">
             <div class="hero-content text-center">
                 <div class="max-w-md">
-                    <h1 class="text-5xl font-bold">Your photos not available yet.</h1>
+                    <h1 class="text-5xl font-bold">Your photos are not available yet.</h1>
                     <p class="py-6">
                         It appears that your photos haven't been uploaded yet. This could be due to a poor or absent internet connection at the photobooth's location.
                         Don't worry, though — you can leave your email address with us, and we'll notify you as soon as your photos are ready.
