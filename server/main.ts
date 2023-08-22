9a3d912b4810ea400aeef10be8b3591b88547fa1
posporus <git@posporus.de>
2023-08-22 23:55:17 +0200

    sendmail

--- server/main.ts
+++ server/main.ts
@@ -3,12 +3,18 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-import "$std/dotenv/load.ts";
+import "$std/dotenv/load.ts"
 
-import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
+import { client, sendmail } from "./utils/sendmail.ts"
 
-import twindPlugin from "$fresh/plugins/twindv1.ts";
-import twindConfig from "./twind.config.ts";
+import { start } from "$fresh/server.ts"
+import manifest from "./fresh.gen.ts"
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+import twindPlugin from "$fresh/plugins/twindv1.ts"
+import twindConfig from "./twind.config.ts"
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] })
+
+//Close email connection
+if (client)
+    await client.close()
