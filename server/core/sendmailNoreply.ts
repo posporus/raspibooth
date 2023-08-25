ed4d0b0028034af691443ac2d07b1c1504bcec64
posporus <git@posporus.de>
2023-08-25 20:33:22 +0200

    mail config waring

--- server/core/sendmailNoreply.ts
+++ server/core/sendmailNoreply.ts
@@ -2,33 +2,35 @@ import { sendmail, type SendmailFunction } from "./mailUtils.ts"
 import { config } from "./config.ts"
 import { SendConfig } from "../deps.ts"
 import { isDefined } from "../utils/typeguards.ts"
+import { logger } from "./logger.ts"
 
 
 export const sendmailNoreply: SendmailFunction = async (templateFn) => {
-    if (!isDefined(config.mail_hostname) || 
-    !isDefined(config.mail_password) || 
-    !isDefined(config.mail_username) || 
-    !isDefined(config.mail_from)) {
-    return;
-}
-    else {
-        const clientOptions = {
-            connection: {
-                hostname: config.mail_hostname,
-                port: config.mail_port,
-                tls: config.mail_tls,
-                auth: {
-                    username: config.mail_username,
-                    password: config.mail_password,
-                },
-            },
-        }
-
-        const sendConfig: SendConfig = {
-            ...templateFn(),
-            from: config.mail_from
-        }
-
-        await sendmail(clientOptions, sendConfig)
+    if (!isDefined(config.mail_hostname) ||
+        !isDefined(config.mail_password) ||
+        !isDefined(config.mail_username) ||
+        !isDefined(config.mail_from)) {
+            logger.warning('mail not configured.')
+        return
     }
+
+    const clientOptions = {
+        connection: {
+            hostname: config.mail_hostname,
+            port: config.mail_port,
+            tls: config.mail_tls,
+            auth: {
+                username: config.mail_username,
+                password: config.mail_password,
+            },
+        },
+    }
+
+    const sendConfig: SendConfig = {
+        ...templateFn(),
+        from: config.mail_from
+    }
+
+    await sendmail(clientOptions, sendConfig)
+
 }
