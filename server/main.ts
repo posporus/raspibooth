92eb32ba21c627f63262f2c0d85a077fafcd7abd
posporus <git@posporus.de>
2023-09-15 14:47:57 +0200

    new fresh config format

--- server/main.ts
+++ server/main.ts
@@ -8,7 +8,6 @@ import "$std/dotenv/load.ts"
 import { start } from "$fresh/server.ts"
 import manifest from "./fresh.gen.ts"
 
-import twindPlugin from "$fresh/plugins/twindv1.ts"
-import twindConfig from "./twind.config.ts"
-
-await start(manifest, { plugins: [twindPlugin(twindConfig)] })
+import config from './fresh.config.ts'
+    
+await start(manifest, config)
