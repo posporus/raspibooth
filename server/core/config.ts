f6a6ad8779470d50d98fd225e60ad678a11d73a6
posporus <git@posporus.de>
2023-10-02 09:51:22 +0200

    figuring out .env issue

--- server/core/config.ts
+++ server/core/config.ts
@@ -1,6 +1,11 @@
 import { loadConfig } from "./configUtils.ts";
 import { configSchema } from "./configSchema.ts";
 
+import { loadDotenv } from "./configUtils.ts"
+
+await loadDotenv()
+
+
 console.log(Deno.env.toObject())
 
 export const _config = () => loadConfig(Deno.env,configSchema)
--- server/core/configUtils.ts
+++ server/core/configUtils.ts
@@ -1,8 +1,10 @@
 import { logger } from "./logger.ts";
 import { load } from "https://deno.land/std@0.202.0/dotenv/mod.ts";
 
+
 export async function loadDotenv(envPath?:string) {
     const env = await load({envPath})
+    logger.info(`Imported env file ${envPath}`)
     for(const key in env) {
         Deno.env.set(key,env[key])
     }
